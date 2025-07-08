6423e429caaf8c45ffc70bc1991c71349fba8696
dperry83 <125750519+dperry83@users.noreply.github.com>
2025-07-08 13:33:45 -0400

    found bug in older new user form.  new users now being saved from command line.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
-  validates :name, presence: true
+  validates :first_name, presence: true
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   def index
